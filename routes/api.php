34b7cfb13105734681ec2a73b901ec2d709b935f
rahulbodara <rb.globalia@gmail.com>
2022-04-18 16:12:00 +0530

    test flow

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::middleware('cors')->group(function () {
-    Route::get('hello', function(){
-        dd("hello world");
-    });
     Route::post('register', [UserController::class, 'register']);
     Route::post('login', [UserController::class, 'login']);
     Route::get('logout', [UserController::class, 'logout']);
